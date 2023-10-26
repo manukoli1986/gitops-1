8dc422233a9dd18eeec91dd1a1aabd1a8569ba84
mike williams <mike2023williams@gmail.com>
2023-10-26 14:21:53 +0530

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import socket
 import datetime
 
-print("Hello, World Gitops!")
+print("Hello,World Gitops!")
 print("The current time is:", datetime.datetime.now())
 print("The hostname is:", socket.gethostname())
